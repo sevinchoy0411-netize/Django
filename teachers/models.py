afd315c787e014fc8496f4a00fa56157493c4a7a
Sevinchoy_ITgirl <sevinchoy0411@gmail.com>
2026-05-19 13:56:17 +0500

    .

--- teachers/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Teacher(models.Model):
-    ismi      = models.CharField(max_length=200)
-    yonalishi = models.CharField(max_length=150)
